bd72c0dcc8d818161e8b8c9f2b19c6be8ee4b862
Javier Agustin Larroca <javier-larroca@aticasrl.com.ar>
2026-08-18 14:49:20 -0300

    2. Clases, Objetos, Propiedades, Encapsulamiento

---
+++ c#-n2-u2.2-clases-objetos-propiedades-encapsulamiento/ejemplo1/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ejemplo1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ejemplo1")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c3d4e5f6-a7b8-4901-c234-56789abcdef0")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
